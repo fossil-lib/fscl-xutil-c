45db7a785e0da22b7014396ddfe3b07bf5d0cd17
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-12-24 02:00:46 -0600

    add ranges

--- code/include/trilobite/xutil/ranges.h
+++ code/include/trilobite/xutil/ranges.h
@@ -45,7 +45,27 @@ extern "C"
 {
 #endif
 
-//
+#include <stdbool.h>
+
+// Represents a range with a minimum and maximum value
+typedef struct {
+    int min;
+    int max;
+} crange;
+
+// =================================================================
+// Avalable functions
+// =================================================================
+bool tscl_crange_contains(const crange *range, int value);
+bool tscl_crange_overlaps(const crange *range1, const crange *range2);
+crange tscl_crange_intersection(const crange *range1, const crange *range2);
+bool tscl_crange_is_empty(const crange *range);
+bool tscl_crange_is_valid(const crange *range);
+crange tscl_crange_create(int min, int max);
+bool tscl_crange_is_subset(const crange *subRange, const crange *superRange);
+bool tscl_crange_is_equal(const crange *range1, const crange *range2);
+crange tscl_crange_union(const crange *range1, const crange *range2);
+crange tscl_crange_expand(const crange *range, int amount);
 
 #ifdef __cplusplus
 }
--- code/source/ranges.c
+++ code/source/ranges.c
@@ -30,3 +30,65 @@
     ----------------------------------------------------------------------------
 */
 #include "trilobite/xutil/ranges.h"
+
+// Function to check if a value is within the specified range
+bool tscl_crange_contains(const crange *range, int value) {
+    return (value >= range->min) && (value <= range->max);
+}
+
+// Function to check if two ranges overlap
+bool tscl_crange_overlaps(const crange *range1, const crange *range2) {
+    return (range1->min <= range2->max) && (range1->max >= range2->min);
+}
+
+// Function to get the intersection of two ranges
+crange tscl_crange_intersection(const crange *range1, const crange *range2) {
+    crange intersection;
+    intersection.min = (range1->min > range2->min) ? range1->min : range2->min;
+    intersection.max = (range1->max < range2->max) ? range1->max : range2->max;
+    return intersection;
+}
+
+// Function to check if a range is empty
+bool tscl_crange_is_empty(const crange *range) {
+    return range->min > range->max;
+}
+
+// Function to check if a range is valid (min <= max)
+bool tscl_crange_is_valid(const crange *range) {
+    return range->min <= range->max;
+}
+
+// Function to create a range with specified minimum and maximum values
+crange tscl_crange_create(int min, int max) {
+    crange range;
+    range.min = min;
+    range.max = max;
+    return range;
+}
+
+// Function to check if a range is a subset of another range
+bool tscl_crange_is_subset(const crange *subRange, const crange *superRange) {
+    return (subRange->min >= superRange->min) && (subRange->max <= superRange->max);
+}
+
+// Function to check if a range is equal to another range
+bool tscl_crange_is_equal(const crange *range1, const crange *range2) {
+    return (range1->min == range2->min) && (range1->max == range2->max);
+}
+
+// Function to calculate the union of two ranges
+crange tscl_crange_union(const crange *range1, const crange *range2) {
+    crange unionRange;
+    unionRange.min = (range1->min < range2->min) ? range1->min : range2->min;
+    unionRange.max = (range1->max > range2->max) ? range1->max : range2->max;
+    return unionRange;
+}
+
+// Function to expand a range by a specified amount
+crange tscl_crange_expand(const crange *range, int amount) {
+    crange expandedRange;
+    expandedRange.min = range->min - amount;
+    expandedRange.max = range->max + amount;
+    return expandedRange;
+}
