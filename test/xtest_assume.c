847aa3af7a89b35bbc3e4a895e294d8d20977d44
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-12-24 01:37:03 -0600

    update tests

--- test/xtest_assume.c
+++ test/xtest_assume.c
@@ -12,64 +12,45 @@
 //
 // XUNIT TEST CASES
 //
-XTEST_CASE(f);
-
-XTEST_CASE(test_tscl_assume_true) {
-    TEST_ASSERT_TRUE(tscl_assume(true, "This should pass."));
+XTEST_CASE(test_tscl_assume_this) {
+    TEST_ASSERT_TRUE(tscl_assume_this(1 == 1));
 }
 
-XTEST_CASE(test_tscl_assume_false) {
-    TEST_ASSERT_FALSE(tscl_assume(false, "This should fail."));
+XTEST_CASE(test_tscl_assume_this_and_that) {
+    TEST_ASSERT_TRUE(tscl_assume_this_and_that(1 == 1, 2 == 2));
 }
 
-XTEST_CASE(test_tscl_assume_not_null_valid) {
-    int* ptr = (int*)malloc(sizeof(int));
-    TEST_ASSERT_TRUE(tscl_assume_not_null(ptr, "This should pass."));
+XTEST_CASE(test_tscl_assume_this_or_that) {
+    TEST_ASSERT_TRUE(tscl_assume_this_or_that(1 == 1, 2 == 2));
+}
+
+XTEST_CASE(test_tscl_assume_this_unless) {
+    TEST_ASSERT_TRUE(tscl_assume_this_unless(1 == 1, 2 == 2));
+}
+
+XTEST_CASE(test_tscl_assume_this_not_that) {
+    TEST_ASSERT_TRUE(tscl_assume_this_not_that(1 == 1, 2 != 1));
+}
+
+XTEST_CASE(test_tscl_assume_range) {
+    TEST_ASSERT_TRUE(tscl_assume_range(5, 0, 10));
+}
+
+XTEST_CASE(test_tscl_assume_not_cnullptr) {
+    int *ptr = malloc(sizeof(int));
+    TEST_ASSERT_TRUE(tscl_assume_not_cnullptr(ptr));
     free(ptr);
-}
-
-XTEST_CASE(test_tscl_assume_not_null_invalid) {
-    TEST_ASSERT_FALSE(tscl_assume_not_null(NULL, "This should fail."));
-}
-
-XTEST_CASE(test_tscl_assume_within_bounds_valid) {
-    size_t size = 5;
-    size_t index = 3;
-    TEST_ASSERT_TRUE(tscl_assume_within_bounds(index, size, "This should pass."));
-}
-
-XTEST_CASE(test_tscl_assume_within_bounds_invalid) {
-    size_t size = 5;
-    size_t index = 7;
-    TEST_ASSERT_FALSE(tscl_assume_within_bounds(index, size, "This should fail."));
-}
-
-XTEST_CASE(test_tscl_assume_positive_valid) {
-    int value = 42;
-    TEST_ASSERT_TRUE(tscl_assume_positive(value, "This should pass."));
-}
-
-XTEST_CASE(test_tscl_assume_positive_invalid) {
-    int value = -5;
-    TEST_ASSERT_FALSE(tscl_assume_positive(value, "This should fail."));
-}
-
-XTEST_CASE(test_tscl_assume_file_not_null_invalid) {
-    FILE* file = NULL;
-    TEST_ASSERT_FALSE(tscl_assume_file_not_null(file, "This should fail."));
 }
 
 //
 // XUNIT-TEST RUNNER
 //
 XTEST_GROUP_DEFINE(test_assume_group) {
-    XTEST_RUN_UNIT(test_tscl_assume_true,  runner);
-    XTEST_RUN_UNIT(test_tscl_assume_false, runner);
-    XTEST_RUN_UNIT(test_tscl_assume_not_null_valid,        runner);
-    XTEST_RUN_UNIT(test_tscl_assume_not_null_invalid,      runner);
-    XTEST_RUN_UNIT(test_tscl_assume_within_bounds_valid,   runner);
-    XTEST_RUN_UNIT(test_tscl_assume_within_bounds_invalid, runner);
-    XTEST_RUN_UNIT(test_tscl_assume_positive_valid,        runner);
-    XTEST_RUN_UNIT(test_tscl_assume_positive_invalid,      runner);
-    XTEST_RUN_UNIT(test_tscl_assume_file_not_null_invalid, runner);
+    XTEST_RUN_UNIT(test_tscl_assume_this, runner);
+    XTEST_RUN_UNIT(test_tscl_assume_this_and_that, runner);
+    XTEST_RUN_UNIT(test_tscl_assume_this_or_that,  runner);
+    XTEST_RUN_UNIT(test_tscl_assume_this_unless,   runner);
+    XTEST_RUN_UNIT(test_tscl_assume_this_not_that, runner);
+    XTEST_RUN_UNIT(test_tscl_assume_range,         runner);
+    XTEST_RUN_UNIT(test_tscl_assume_not_cnullptr,  runner);
 } // end of func
