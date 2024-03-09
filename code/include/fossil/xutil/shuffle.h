24f43fce7050fa938ad21849f9acc876fddcd942
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 12:01:55 -0600

    Update shuffle.h

--- code/include/fossil/xutil/shuffle.h
+++ code/include/fossil/xutil/shuffle.h
@@ -24,11 +24,52 @@ extern "C"
 // =================================================================
 // Avalable functions
 // =================================================================
+
+/**
+ * Seed the randomization for shuffle functions.
+ *
+ * @param seed The seed value for randomization.
+ */
 void fscl_shuffle_seed(unsigned int seed);
+
+/**
+ * Shuffle an array of integers.
+ *
+ * @param array The array to shuffle.
+ * @param size  The size of the array.
+ */
 void fscl_shuffle_int(int* array, size_t size);
+
+/**
+ * Shuffle an array of double-precision floating-point numbers.
+ *
+ * @param array The array to shuffle.
+ * @param size  The size of the array.
+ */
 void fscl_shuffle_double(double* array, size_t size);
+
+/**
+ * Shuffle an array of C-style strings.
+ *
+ * @param array The array of strings to shuffle.
+ * @param size  The size of the array.
+ */
 void fscl_shuffle_cstring(char** array, size_t size);
+
+/**
+ * Shuffle an array of characters.
+ *
+ * @param array The array of characters to shuffle.
+ * @param size  The size of the array.
+ */
 void fscl_shuffle_letter(char* array, size_t size);
+
+/**
+ * Shuffle an array of boolean values.
+ *
+ * @param array The array of boolean values to shuffle.
+ * @param size  The size of the array.
+ */
 void fscl_shuffle_bool(bool* array, size_t size);
 
 #ifdef __cplusplus
