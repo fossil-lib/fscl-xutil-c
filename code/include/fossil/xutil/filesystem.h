919a655058d6adb8b46992dbf05cbd107d2acb15
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 09:53:51 -0600

    Update filesystem.h

--- code/include/fossil/xutil/filesystem.h
+++ code/include/fossil/xutil/filesystem.h
@@ -26,12 +26,59 @@ typedef struct {
 // =================================================================
 // Avalable functions
 // =================================================================
+
+/**
+ * Create a filesystem object for the specified path.
+ *
+ * @param path The path for the filesystem.
+ * @return     The created filesystem object.
+ */
 cfilesystem fscl_filesys_create(const char* path);
+
+/**
+ * Erase a filesystem object.
+ *
+ * @param directory The filesystem object to be erased.
+ */
 void fscl_filesys_erase(cfilesystem* directory);
+
+/**
+ * List files in the specified directory.
+ *
+ * @param directory The directory to list files from.
+ */
 void fscl_filesys_list_files(const cfilesystem* directory);
+
+/**
+ * Create a subdirectory in the specified parent directory.
+ *
+ * @param parent            The parent directory.
+ * @param subfscl_filesys_name The name of the subdirectory to be created.
+ */
 void fscl_filesys_create_subdirectory(const cfilesystem* parent, const char* subfscl_filesys_name);
+
+/**
+ * Check if a directory or filesystem exists at the specified path.
+ *
+ * @param directory The directory or filesystem to check.
+ * @return          1 if it exists, 0 otherwise.
+ */
 int fscl_filesys_exists(const cfilesystem* directory);
+
+/**
+ * Remove a file from the specified directory.
+ *
+ * @param directory The directory from which to remove the file.
+ * @param filename  The name of the file to be removed.
+ */
 void fscl_filesys_remove_file(const cfilesystem* directory, const char* filename);
+
+/**
+ * Change the current working directory of the filesystem object.
+ *
+ * @param directory The filesystem object to change the directory for.
+ * @param new_path  The new path to set as the working directory.
+ */
 void fscl_filesys_change_directory(cfilesystem* directory, const char* new_path);
 
 #ifdef __cplusplus
