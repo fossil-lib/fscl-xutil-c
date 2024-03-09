22d40ef75c21ccbc98413b684160c8e6ca543490
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 09:54:50 -0600

    Update lavalamp.h

--- code/include/fossil/xutil/lavalamp.h
+++ code/include/fossil/xutil/lavalamp.h
@@ -27,19 +27,99 @@ typedef struct {
 // =================================================================
 // Avalable functions
 // =================================================================
-clavalamp* fscl_lava_create(int numLamps);
-void fscl_lava_erase(clavalamp* lamps);
-void fscl_lava_capture_state(clavalamp* lamp);
-void fscl_lava_combine_states(clavalamp* lamps, int numLamps, char* combinedState);
-unsigned int fscl_lava_hash_combined_state(const char* combinedState, int length);
-void fscl_lava_print_state(const clavalamp* lamp);
-void fscl_lava_visualize(const clavalamp* lamps, int numLamps);
-void fscl_lava_analyze(const clavalamp* lamps, int numLamps);
-void fscl_lava_reset(clavalamp* lamps, int numLamps);
-void fscl_lava_randomize(clavalamp* lamps, int numLamps);
-void fscl_lava_seed(unsigned int seed);
-int fscl_lava_random(void);
 
+/**
+ * Create an array of lava lamps with the specified number of lamps.
+ *
+ * @param numLamps The number of lava lamps to create.
+ * @return         Pointer to the created array of lava lamps.
+ */
+clavalamp* fscl_lava_create(int numLamps);
+
+/**
+ * Erase an array of lava lamps.
+ *
+ * @param lamps Pointer to the array of lava lamps to be erased.
+ */
+void fscl_lava_erase(clavalamp* lamps);
+
+/**
+ * Capture the state of a lava lamp.
+ *
+ * @param lamp Pointer to the lava lamp to capture the state.
+ */
+void fscl_lava_capture_state(clavalamp* lamp);
+
+/**
+ * Combine the states of multiple lava lamps into a single string.
+ *
+ * @param lamps         Pointer to the array of lava lamps.
+ * @param numLamps      The number of lava lamps.
+ * @param combinedState Buffer to store the combined state string.
+ */
+void fscl_lava_combine_states(clavalamp* lamps, int numLamps, char* combinedState);
+
+/**
+ * Hash the combined state string of lava lamps.
+ *
+ * @param combinedState The combined state string to hash.
+ * @param length        The length of the combined state string.
+ * @return              The hash value of the combined state.
+ */
+unsigned int fscl_lava_hash_combined_state(const char* combinedState, int length);
+
+/**
+ * Print the current state of a lava lamp.
+ *
+ * @param lamp Pointer to the lava lamp to print the state.
+ */
+void fscl_lava_print_state(const clavalamp* lamp);
+
+/**
+ * Visualize the states of multiple lava lamps.
+ *
+ * @param lamps    Pointer to the array of lava lamps.
+ * @param numLamps The number of lava lamps.
+ */
+void fscl_lava_visualize(const clavalamp* lamps, int numLamps);
+
+/**
+ * Analyze the states of multiple lava lamps.
+ *
+ * @param lamps    Pointer to the array of lava lamps.
+ * @param numLamps The number of lava lamps.
+ */
+void fscl_lava_analyze(const clavalamp* lamps, int numLamps);
+
+/**
+ * Reset the states of multiple lava lamps.
+ *
+ * @param lamps    Pointer to the array of lava lamps.
+ * @param numLamps The number of lava lamps.
+ */
+void fscl_lava_reset(clavalamp* lamps, int numLamps);
+
+/**
+ * Randomize the states of multiple lava lamps.
+ *
+ * @param lamps    Pointer to the array of lava lamps.
+ * @param numLamps The number of lava lamps.
+ */
+void fscl_lava_randomize(clavalamp* lamps, int numLamps);
+
+/**
+ * Seed the random number generator for lava lamps.
+ *
+ * @param seed The seed value for the random number generator.
+ */
+void fscl_lava_seed(unsigned int seed);
+
+/**
+ * Generate a random number for lava lamps.
+ *
+ * @return The generated random number.
+ */
+int fscl_lava_random(void);
 
 #ifdef __cplusplus
 }
