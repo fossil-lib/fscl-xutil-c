0fcfa7c130477d3fd0793897be7b87c2785b050e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-09 09:57:20 -0600

    Update platform.h

--- code/include/fossil/xutil/platform.h
+++ code/include/fossil/xutil/platform.h
@@ -35,12 +35,57 @@ typedef struct {
 // =================================================================
 // Avalable functions
 // =================================================================
+
+/**
+ * Get information about the operating system.
+ *
+ * @return Information about the operating system.
+ */
 cos_info fscl_platform_get_os_info(void);
+
+/**
+ * Get information about the architecture.
+ *
+ * @return Information about the architecture.
+ */
 carch_info fscl_platform_get_architecture_info(void);
+
+/**
+ * Check if the platform is little-endian.
+ *
+ * @return 1 if the platform is little-endian, 0 otherwise.
+ */
 int fscl_platform_is_little_endian(void);
+
+/**
+ * Check if the platform is big-endian.
+ *
+ * @return 1 if the platform is big-endian, 0 otherwise.
+ */
 int fscl_platform_is_big_endian(void);
+
+/**
+ * Swap the endianess of a 16-bit value.
+ *
+ * @param value The 16-bit value to swap.
+ * @return      The 16-bit value with swapped endianess.
+ */
 uint16_t fscl_platform_swap_endian_16(uint16_t value);
+
+/**
+ * Swap the endianess of a 32-bit value.
+ *
+ * @param value The 32-bit value to swap.
+ * @return      The 32-bit value with swapped endianess.
+ */
 uint32_t fscl_platform_swap_endian_32(uint32_t value);
+
+/**
+ * Swap the endianess of a 64-bit value.
+ *
+ * @param value The 64-bit value to swap.
+ * @return      The 64-bit value with swapped endianess.
+ */
 uint64_t fscl_platform_swap_endian_64(uint64_t value);
 
 #ifdef __cplusplus
